15414fcf9a55c5bdcad2930c1597dadcaf66d9a9
Dylan Cai <dc306@duke.edu>
2022-09-05 12:32:26 -0400

    created blood calculator function

---
+++ blood_calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 12:28:11 2022
+
+@author: dylancai
+"""
+
+def interface():
+    print("Blood Calculator")
+    print("Options:")
+    print("9-Quit")
+    choice = input("Enter choice: ")
+    if choice == "9":
+        return
+    
+interface()
