3b7f37f78c88501a6389908d72aaebe8a9fef145
Dylan Cai <dc306@duke.edu>
2022-09-12 13:04:35 -0400

    split up dosing.py into functions

--- dosing.py
+++ dosing.py
@@ -14,7 +14,7 @@ Created on Mon Sep 12 12:04:41 2022
 """
 
 
-def dose_amount():
+def interface():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -23,6 +23,8 @@ def dose_amount():
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
     diagnosis = int(input("Enter a number: "))
+    return(diagnosis)
+def weight_input():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
@@ -32,15 +34,19 @@ def dose_amount():
     units = weight_data[1]
     if units == "lb":
         weight = weight / 2.205
+    return(weight, units)
+def dosing_calc(diag, weight):
     dosages_mg_per_kg = [30, 10, 10, 12]
-    dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
+    dosage_mg_per_kg = dosages_mg_per_kg[diag-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
           .format(dosage_mg_first_day))
-
+    
 
 if __name__ == '__main__':
-    dose_amount()
+    diagnosis = interface()
+    (weight, units) = weight_input()
+    dosing_calc(diagnosis, weight)
 
