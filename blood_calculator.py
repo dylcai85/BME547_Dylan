49739d70ab76ef9a4bc4e8738e5f5f106f139828
Dylan Cai <dc306@duke.edu>
2022-09-19 13:13:31 -0400

    added tests for LDL function

--- blood_calculator.py
+++ blood_calculator.py
@@ -6,6 +6,7 @@ Created on Mon Sep  5 12:28:11 2022
 @author: dylancai
 """
 
+
 def interface():
     print("Blood Calculator")
     print("Options:")
@@ -24,10 +25,13 @@ def interface():
             LDL_driver()
         elif choice == "3":
             chol_driver()
+
+
 def input_HDL():
     num = input("Enter the HDL value: ")
     return int(num)
-    
+
+
 def check_HDL(hdl_val):
     if hdl_val >= 60:
         return "Normal"
@@ -35,19 +39,23 @@ def check_HDL(hdl_val):
         return "Borderline Low"
     else:
         return "Low"
-    
+
+
 def HDL_driver():
     hdl_val = input_HDL()
     answer = check_HDL(hdl_val)
     output_HDL_result(hdl_val, answer)
 
+
 def output_HDL_result(hdl_value, charac):
     print("The results for an HDL value of {} is {}".format(hdl_value, charac))
+
 
 def input_LDL():
     num = input("Enter the LDL value: ")
     return int(num)
-    
+
+
 def check_LDL(ldl_val):
     if ldl_val >= 190:
         return "very high"
@@ -57,35 +65,42 @@ def check_LDL(ldl_val):
         return "borderline high"
     else:
         return "normal"
-    
+
+
 def LDL_driver():
     ldl_val = input_LDL()
     answer = check_LDL(ldl_val)
     output_LDL_result(ldl_val, answer)
 
+
 def output_LDL_result(ldl_value, charac):
     print("The results for an LDL value of {} is {}".format(ldl_value, charac))
-    
+
+
 def input_chol():
     num = input("Enter the Cholesterol value: ")
     return int(num)
 
+
 def check_chol(chol_val):
-    if chol_val<200:
+    if chol_val < 200:
         return "Normal"
-    elif 200<= chol_val < 240:
+    elif 200 <= chol_val < 240:
         return "Borderline High"
     else:
         return "High"
+
 
 def chol_driver():
     chol_val = input_chol()
     answer = check_chol(chol_val)
     output_chol_result(chol_val, answer)
 
+
 def output_chol_result(chol_val, charac):
-    print("The results for a cholesterol value of {} is {}".format(chol_val, charac))
-   
+    print("The results for a cholesterol value of {} is {}"
+          .format(chol_val, charac))
+
+
 if __name__ == "__main__":
     interface()
-
--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -6,28 +6,24 @@ Created on Mon Sep 19 12:10:41 2022
 @author: dylancai
 """
 import pytest
-@pytest.mark.parametrize("HDL_val, expected", [(85, "Normal"), 
-                                               (40, "Borderline Low"), 
+
+
+@pytest.mark.parametrize("HDL_val, expected", [(85, "Normal"),
+                                               (40, "Borderline Low"),
                                                (20, "Low")
                                                ])
-
-
-def test_check_HDL_Normal(HDL_val, expected):
+def test_check_HDL(HDL_val, expected):
     from blood_calculator import check_HDL
     answer = check_HDL(HDL_val)
     assert answer == expected
 
 
-"""    
-def test_check_HDL_BorderlineLow():
-    from blood_calculator import check_HDL
-    answer= check_HDL(40)
-    expected = "Borderline Low"
+@pytest.mark.parametrize("LDL_val, expected", [(195, "very high"),
+                                               (160, "high"),
+                                               (130, "borderline high"),
+                                               (129, "normal")
+                                               ])
+def test_check_LDL(LDL_val, expected):
+    from blood_calculator import check_LDL
+    answer = check_LDL(LDL_val)
     assert answer == expected
-    
-def test_check_HDL_Low():
-    from blood_calculator import check_HDL
-    answer= check_HDL(20)
-    expected = "Low"
-    assert answer == expected
-"""
