394e53325086fd0c1e490313a3e2926f8c3fedbe
Dylan Cai <dc306@duke.edu>
2022-09-21 12:56:14 -0400

    made class examples folder with weight entry example and also made .github folder with workflow

---
+++ class examples/return_example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 12:18:36 2022
+
+@author: dylancai
+"""
+
+def increment_by_five(x):
+    a = x+5
+    if x<0:
+        return "This function cannot accept answers less than zero"
+    return a
+
+answer = increment_by_five(-11)
+print("The answer is {}".format(answer))
