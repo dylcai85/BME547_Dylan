f81045acdce0d23518bf66c0e100b3e09800017f
Dylan Cai <dc306@duke.edu>
2022-09-26 12:49:04 -0400

    converted database from a list of patients to a dictionary of patients with id as the key

--- database.py
+++ database.py
@@ -12,10 +12,10 @@ def create_patient_entry(patient_first_name, patient_last_name, patient_id, pati
     return new_patient
 
 def main():
-    db = []
-    db.append(create_patient_entry("Ann", "Ables", 1, 30))
-    db.append(create_patient_entry("Bob", "Boyles", 2, 34))
-    db.append(create_patient_entry("Chris", "Chou", 3, 25))
+    db = {}
+    db[1] = create_patient_entry("Ann", "Ables", 1, 30)
+    db[2] = create_patient_entry("Bob", "Boyles", 2, 34)
+    db[3] = create_patient_entry("Chris", "Chou", 3, 25)
     db_print(db)
     test_add(db,3,"HDL", 100)
     db_print(db)
@@ -26,10 +26,10 @@ def main():
     
 def db_print(db):
     for x in db:
-        print("Name: {}, ID: {}, Age: {}, Tests: {}".format(get_full_name(x),
-                                                            x["pid"],
-                                                            x["age"],
-                                                            x['tests']))
+        print("Name: {}, ID: {}, Age: {}, Tests: {}".format(get_full_name(db[x]),
+                                                            db[x]["pid"],
+                                                            db[x]["age"],
+                                                            db[x]['tests']))
 
 
 def adult_or_minor(patient):
@@ -43,10 +43,9 @@ def get_full_name(patient):
     return("{} {}".format(patient["firstname"], patient["lastname"]))
         
 def find_patient(db, id):
-    for p in db:
-        if p.get('pid') == id:
-            return p
-    return False
+    patient = db[id]
+    return patient
+
 
 def test_add(db, id, test_name, test_val):
     patient = find_patient(db, id)
